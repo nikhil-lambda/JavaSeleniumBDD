fc6b24b16049e39d2d156beba2f9e2edaf6c5d65
mazuma5 <48091471+mazuma5@users.noreply.github.com>
2019-03-12 18:03:49 +0530

    Update ConnectionSteps.java

--- src/main/java/xyz/aingaran/framework/steps/ConnectionSteps.java
+++ src/main/java/xyz/aingaran/framework/steps/ConnectionSteps.java
@@ -13,12 +13,12 @@ public class ConnectionSteps {
         Framework.init();
     }
 
-    @When("user navigates to google.com")
+    @When("navigates to http://34.221.37.187:3000/")
     public void user_navigates_to_google_com() {
         Framework.getWebDriver().navigate().to("http://34.221.37.187:3000/");
     }
 
-    @Then("user sees element app logo")
+    @Then("user sees element image1")
     public void user_sees_element_hplogo() {
         Framework.getWebDriver().findElement(By.id("image1"));
     }
@@ -28,12 +28,17 @@ public class ConnectionSteps {
         Framework.takeScreenShot("pwa", "page1");
     }
     
-     @When("user navigates to google.com")
+    @Given("user loads browser")
+    public void user_loads_browser() {
+        Framework.init();
+    }
+    
+     @When("navigates to http://34.221.37.187:3000/")
     public void user_navigates_to_google_com() {
         Framework.getWebDriver().navigate().to("http://34.221.37.187:3000/");
     }
 
-    @Then("user sees element app logo")
+    @Then("user sees element link1")
     public void user_sees_element_hplogo() {
         Framework.getWebDriver().findElement(By.id("link1"));
     }
@@ -43,18 +48,23 @@ public class ConnectionSteps {
         Framework.takeScreenShot("pwa", "page2");
     }
     
-     @When("user navigates to google.com")
+    @Given("user loads browser")
+    public void user_loads_browser() {
+        Framework.init();
+    }
+    
+     @When("navigates to http://34.221.37.187:3000/")
     public void user_navigates_to_google_com() {
         Framework.getWebDriver().navigate().to("http://34.221.37.187:3000/");
     }
 
-    @Then("user sees element app logo")
+    @Then("user sees element link2")
     public void user_sees_element_hplogo() {
         Framework.getWebDriver().findElement(By.id("link2"));
     }
 
     @Then("user takes a screenshot")
     public void user_takes_a_screenshot() {
-        Framework.takeScreenShot("pwa", "page2");
+        Framework.takeScreenShot("pwa", "page3");
     }
 }
