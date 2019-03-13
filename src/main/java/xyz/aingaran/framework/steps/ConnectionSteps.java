b3224f3e95044efccb10084c404405751ecbfad4
mazuma5 <48091471+mazuma5@users.noreply.github.com>
2019-03-13 15:05:38 +0530

    Update ConnectionSteps.java

--- src/main/java/xyz/aingaran/framework/steps/ConnectionSteps.java
+++ src/main/java/xyz/aingaran/framework/steps/ConnectionSteps.java
@@ -55,7 +55,7 @@ public class ConnectionSteps {
         try{
             Framework.getWebDriver().findElement(By.id("link3"));
         }catch(Exception e){
-            Assert.assertFalse(true);
+            Assert.assertFalse(false);
         } 
     } 
     
